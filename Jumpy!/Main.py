bbff3dd98cb1e6ba9268b672689ba982b404ec7c
Falk Hoeppner <41196937+falkhop@users.noreply.github.com>
2019-12-04 00:12:04 -0600

    Initial upload
    Upload of what is basically the completed game.  I have a few options of things I may want to add, but nothing too crazy.

---
+++ Jumpy!/Main.py
@@ -0,0 +1,214 @@
+# This is the video series I started this with: https://www.youtube.com/watch?v=uWvb3QzA48c&t=3s
+# Jumpy! (a platform game)
+# Art from Kenney.nl
+# Happy Tune by https://opengameart.org/content/happy-tune
+# Acrostics bu Cityfires
+
+import pygame as pg
+import random
+from settings import *
+import time
+from sprites import *
+from os import path
+import os
+
+class Game:
+    def __init__(self):
+        # initialize game window
+        pg.init()
+        pg.mixer.init()
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption(TITLE)
+        self.clock = pg.time.Clock()
+        self.running = True
+        self.font_name = pg.font.match_font(FONT_NAME)
+        self.load_data()
+
+
+    def load_data(self):
+        # load high score
+        self.dir = path.dirname(__file__)
+        img_dir = path.join(self.dir, 'img')
+        with open(path.join(self.dir, HS_FILE), 'w') as f:
+            try:
+                self.highscore = int(f.read())
+            except:
+                self.highscore = 0
+        # load spritesheet image
+        self.spritesheet = Spritesheet(path.join(img_dir, SPRITESHEET))
+        # load sounds
+        self.snd_dir = path.join(self.dir, 'snd')
+        self.jump_sound = pg.mixer.Sound(path.join(self.snd_dir, 'Jump2.wav'))
+        # load misc
+        misc_dir = path.join(self.dir, 'misc')
+
+
+    def new(self):
+        # start a new game
+        self.score = 0
+        self.all_sprites = pg.sprite.Group()
+        self.platforms = pg.sprite.Group()
+        self.player = Player(self)
+        self.all_sprites.add(self.player)
+        # add in the platforms
+        for plat in PLATFORM_LIST:
+            p = Platform(self, *plat)
+            self.all_sprites.add(p)
+            self.platforms.add(p)
+        pg.mixer.music.load(path.join(self.snd_dir, 'happytune.wav'))
+        self.run()
+
+    def run(self):
+        # Game Loop
+        pg.mixer.music.play(loops=-1)
+        self.playing = True
+        while self.playing:
+            self.clock.tick(FPS)
+            self.events()
+            self.update()
+            self.draw()
+        pg.mixer.music.fadeout(1000)
+
+    def update(self):
+        # Game Loop Update
+        self.all_sprites.update()
+        # check if player hits a platform - only if falling
+        if self.player.vel.y > 0:
+            hits = pg.sprite.spritecollide(self.player, self.platforms, False)
+            if hits:
+                lowest = hits[0]
+                for hit in hits:
+                    if hit.rect.bottom > lowest.rect.bottom:
+                        lowest = hit
+                if self.player.pos.y < lowest.rect.centery:
+                    self.player.pos.y = lowest.rect.top + 1
+                    self.player.vel.y = 0
+                    self.player.jumping = False
+
+        # if player reaches top 1/4 of screen
+        if self.player.rect.top <= HEIGHT / 4:
+            self.player.pos.y += max(abs(self.player.vel.y), 2.5)
+            for plat in self.platforms:
+                plat.rect.y += max(abs(self.player.vel.y), 2.5)
+                if plat.rect.top >= HEIGHT:
+                    plat.kill()
+                    self.score += 1
+        # DEATH!
+        if self.player.rect.bottom > HEIGHT:
+            for sprite in self.all_sprites:
+                sprite.rect.y -= max(self.player.vel.y, 10)
+                if sprite.rect.bottom < 0:
+                    sprite.kill()
+        if len(self.platforms) == 0:
+            self.playing = False
+
+        # spawn new platforms to keep same average number of platforms
+        while len(self.platforms) < 6:
+            width = random.randrange(50, 100)
+            p = Platform(self, random.randrange(0, WIDTH-width),
+                         random.randrange(-75, -30))
+            self.platforms.add(p)
+            self.all_sprites.add(p)
+
+    def events(self):
+        # Game Loop Events
+        for event in pg.event.get():
+            # check for closing window
+            if event.type == pg.QUIT:
+                if self.playing:
+                    self.playing = False
+                self.running = False
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_SPACE:
+                    self.player.jump()
+            if event.type == pg.KEYUP:
+                if event.key == pg.K_SPACE:
+                    self.player.jump_cut()
+
+    def draw(self):
+        # Game Loop Draw
+        self.screen.fill(BGCOLOR)
+        self.all_sprites.draw(self.screen)
+        self.screen.blit(self.player.image, self.player.rect)
+        # Scoreboard
+        self.draw_text(str(self.score), 22, WHITE, WIDTH/2, 15)
+        # after drawing everything, flip the display
+        pg.display.flip()
+
+    def show_start_screen(self):
+        # Game Splash/Start screen
+        pg.mixer.music.load(path.join(self.snd_dir, 'happytune.wav'))
+        pg.mixer.music.play(loops=-1)
+        self.screen.fill(BGCOLOR)
+        self.draw_text(TITLE, 48, WHITE, WIDTH / 2, HEIGHT / 4)
+        self.draw_text("Arrows to move, Space to jump!", 20, WHITE, WIDTH / 2, HEIGHT / 2)
+        self.draw_text("Press any key to play", 20, WHITE, WIDTH / 2, HEIGHT * 5 / 8)
+        self.draw_text("High Score: " + str(self.highscore), 20, WHITE, WIDTH / 2, HEIGHT * 6 / 8)
+        pg.display.flip()
+        self.wait_for_key()
+        pg.mixer.music.fadeout(500)
+
+    def slow_type(self):
+        secret = open(path.join(self.dir, SCRT_FILE), 'r')
+        for x in secret:
+            print(x)
+            time.sleep(SLEEP)
+
+    def instructions(self):
+        self.screen.fill(BLACK)
+        font = pg.font.SysFont("americantypewriter", 16)
+        for n, line in enumerate(instructionsFile):
+            text = font.render(line, 1, WHITE)
+            text_rect = text.get_rect()
+            text_rect.centerx = 100
+            text_rect.centery = n * 25 + 50
+            self.screen.blit(text, text_rect)
+            time.sleep(SLEEP)
+            pg.display.update()
+
+    def show_go_screen(self):
+        # Game Over/Continue screen
+        pg.mixer.music.load(path.join(self.snd_dir, 'happytune.wav'))
+        pg.mixer.music.play(loops=-1)
+        if not self.running:
+            return
+        self.screen.fill(BGCOLOR)
+        self.draw_text("GAME OVER!", 48, WHITE, WIDTH / 2, HEIGHT / 4)
+        self.draw_text("Score: " + str(self.score), 20, WHITE, WIDTH / 2, HEIGHT / 2)
+        self.draw_text("Press any key to play again", 20, WHITE, WIDTH / 2, HEIGHT * 5 / 8)
+        # self.instructions()
+        if self.score > self.highscore:
+            self.highscore = self.score
+            self.draw_text("NEW HIGH SCORE!", 20, WHITE, WIDTH / 2, HEIGHT /2 + 40)
+            with open(path.join(self.dir, HS_FILE), 'w') as f:
+                f.write(str(self.score))
+        else:
+            self.draw_text("High Score: " + str(self.highscore), 20, WHITE, WIDTH / 2, HEIGHT /2 + 40)
+        pg.display.flip()
+        self.wait_for_key()
+
+    def wait_for_key(self):
+        waiting = True
+        while waiting:
+            self.clock.tick(FPS)
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    waiting = False
+                    self.running = False
+                if event.type == pg.KEYUP:
+                    waiting = False
+
+    def draw_text(self, text, size, color, x, y):
+        font = pg.font.Font(self.font_name, size)
+        text_surface = font.render(text, True, color)
+        text_rect = text_surface.get_rect()
+        text_rect.midtop = (x,y)
+        self.screen.blit(text_surface, text_rect)
+
+g = Game()
+g.show_start_screen()
+while g.running:
+    g.new()
+    g.show_go_screen()
+
+pg.quit()
